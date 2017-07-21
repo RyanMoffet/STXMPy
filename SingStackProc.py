265fb30371045059125918a4c5d3284cdd8a9774
RyanMoffet <ryanmoffet@gmail.com>
2017-07-21 14:52:48 -0700

    alignment routine updates

--- SingStackProc.py
+++ SingStackProc.py
@@ -10,6 +10,7 @@ Created on Wed Jun  7 14:10:04 2017
 
 import os
 import math
+import cmath
 
 import numpy as np
 
@@ -42,23 +43,8 @@ plugin = file_plugins.identify(filepath) # dont quite know what this is for ...
 stack = stk
 file_plugins.load(filepath, stk, plugin=plugin,selection=FileInternalSelection)
 #------------------------------------------------------------------------------
-#def align_stack(stack):
-stackcontainer = stack.absdata
 
-dims = np.shape(stackcontainer)
-
-xresloution = np.mean(np.diff(stack.x_dist))
-yresolution = np.mean(np.diff(stack.x_dist))
-center = np.ceil(stack.n_ev/4*3)
-
-spectr = np.zeros(dims)
-
-shifts = np.zeros((dims[2], 2))
-
-for k in range(dims[2]):   
-    shifts[k,:], err, phasediff = register_translation(stackcontainer[:,:,int(center)],
-          stackcontainer[:,:,k], 50)
-
+    
 def ft_matrix_shift(A,dy,dx):
     # shifts matrix elements (not-integer shifts dx,dy are possible)
     # follows from TR Henn's original code...
@@ -67,7 +53,18 @@ def ft_matrix_shift(A,dy,dx):
     fx = ((range(Nx)-rx)/(Nx/2))
     ry = np.floor(Ny/2)+1
     fy = ((range(Ny)-ry)/(Ny/2))
-    px = np.fft.ifftshift(math.exp())
+
+    px = np.fft.ifftshift(np.exp(-1j*dx*cmath.pi*fx))
+    py = np.fft.ifftshift(np.exp(-1j*dy*cmath.pi*fy))
+    
+    yphase, xphase = np.meshgrid(py,px)
+    
+    yphase = np.transpose(yphase)
+    xphase = np.rot90(xphase)
+    
+    B = abs(np.fft.ifft2(np.fft.fft2(A))*yphase*xphase)
+    
+    return B
 #----------------------------------------------------------------------        
 def show_image(iev, stk):
     
@@ -106,8 +103,43 @@ def deglitch_stack(stack, iev):
         iev = iev-1
         if iev < 0:
             iev = 0
+            
+            
+# def align_stack(stack):
+stackcontainer = stack.absdata
+
+dims = np.shape(stackcontainer)
+
+ymax = dims[0]
+xmax = dims[1]
+emax = dims[2]
+
+xresloution = np.mean(np.diff(stack.x_dist))
+yresolution = np.mean(np.diff(stack.x_dist))
+center = np.ceil(stack.n_ev/4*3)
+
+spectr = np.zeros(dims)
+
+shifts = np.zeros((dims[2], 2))
+
+for k in range(dims[2]):   
+    shifts[k,:], err, phasediff = register_translation(stackcontainer[:,:,int(center)],
+          stackcontainer[:,:,k], 50)
+    spectr[:,:,k] = ft_matrix_shift(stackcontainer[:,:,k],-shifts[k,0],-shifts[k,1])            
+
+shiftymax = np.ceil(np.max(shifts[:,0]))
+shiftxmax = np.ceil(np.max(shifts[:,1]))
+shiftymin = np.ceil(np.abs(np.min(shifts[:,0])))
+shiftxmin = np.ceil(np.abs(np.min(shifts[:,1])))
+
+shiftmatrix = np.zeros((int(ymax-shiftymin-shiftymax),int(xmax-shiftxmax-shiftxmin)
+,int(emax)))
+
 #---------------------------------------------------------------------------
 # http://scikit-image.org/docs/dev/auto_examples/transform/plot_register_translation.html#sphx-glr-auto-examples-transform-plot-register-translation-py
+
+
+
 
 
 show_image(20, stk)
@@ -117,6 +149,8 @@ deglitch_stack(stk,3)
     
 ani = stack_movie(stk)
 plt.show()
+
+
 #------------------------------------------------------------------------------
 # work on animating images...
 
